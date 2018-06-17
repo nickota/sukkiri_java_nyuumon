6fb3e367ca79ccc1d7cd9da0320ed5d624b8c54b
Kota Takeda <xxk0ta0605@gmail.com>
2018-06-17 12:20:06 +0000

    p.491

--- src/CleaningService.java
+++
@@ -1,5 +0,0 @@
-public interface CleaningService{
-    public abstract Shirt washShirt(Shirt s);  //練習のため残してるが、public abstractは省略可能
-    public abstract Towl washTowl(Towl t);
-    public abstract Coat washCoat(Coat c);
-}
---
+++ src/Computer.java
@@ -0,0 +1,15 @@
+public class Computer extends TangibleAsset{
+    private String name;
+    private int price;
+    private String color;
+    private String makerName;
+    
+    Computer(String name, int price, String color, String makerName){
+        super(name, price, color);
+        this.makerName = makerName;
+    }
+    
+    public String getMakerName(){
+        this.makerName = getMakerName;
+    }
+}
