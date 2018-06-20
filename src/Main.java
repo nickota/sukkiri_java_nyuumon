bd35a222f74eed5b81a6b9138ba37d8e2ae4cf30
Kota Takeda <xxk0ta0605@gmail.com>
2018-06-20 13:19:35 +0000

    p.520_多態性の利点

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,13 @@
 public class Main{
     public static void main(String[] args){
-        Wizard w = new Wizard();
-        Matango m = new Matango();
-        w.name = "アサカ";
-        w.attack(m);
-        w.fireball(m);
+        Character[] c = new Character[5];
+        c[0] = new Hero;
+        c[1] = new Hero;
+        c[2] = new Wizard;
+        ...
+        //別々の型に対して同じ配列にいれることができて、同一の処理を行うことができる
+        for(Character ch : c){
+            ch.setHp(ch.getHp() =+ 50);
+        }
     }
 }
