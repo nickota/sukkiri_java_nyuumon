23b9c465eb7ef9531053392eceb71ba8b3ad884b
Kota Takeda <xxk0ta0605@gmail.com>
2018-06-21 13:08:33 +0000

    p.539

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,17 @@
+import java.util.Calendar;
 import java.util.Date;
 public class Main{
     public static void main (String[] args){
+        //現在の年を表示
         Date now = new Date();
-        System.out.println(now);
-        System.out.println(now.getTime());
-        Date past = new Date(131662222593L);
-        System.out.println(past);
+        Calendar c = Calendar.getInstance();
+        c.setTime(now);
+        
+        int y = c.get(Calendar.YEAR);
+        System.out.println("今年は" + y + "年です");
+        //指定した日のDate型の値をとる
+        c.set(2010,8,22,1,23,45);
+        c.set(Calendar.YEAR, 2011);
+        Date past = c.getTime();
     }
 }
