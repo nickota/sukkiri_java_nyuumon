7786984727b8615f106ee6fe59c6ea86655cee22
Kota Takeda <xxk0ta0605@gmail.com>
2018-06-23 09:00:28 +0000

    p,559

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,24 @@
+import java.util.Date;
+import java.util.Calendar;
+import java.text.SimpleDateFormat;
+
 public class Main {
-    public void printAnything(Object o) {
-        //何型でも表示することができる
-        System.out.println(o);
+    public static void main(String[] args) {
+        //1
+        Date now = new Date();
+        //2
+        Calendar c = Calendar.getInstance();
+        c.setTime(now);
+        //3
+        int day = c.get(Calendar.DAY_OF_MONTH);
+        //4
+        day =+ 100;
+        c.set(Calendar.DAY_OF_MONTH, day);
+        //5
+        Date future = c.getTime();
+        //6
+        SimpleDateFormat f = new SimpleDateFormat("西暦yyyy年MM月dd日");
+        System.out.println(f.format(future));
+    }
+        
 }
