892a5b2b5b0d36d4728ac8ca246ef61318c2ee8b
Kota Takeda <xxk0ta0605@gmail.com>
2018-06-23 10:26:23 +0000

    p.584

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,15 @@
 import java.io.*;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args){
         try {
             FileWriter fw = new FileWriter("data.txt");
-        } catch(IOException e) {
+            fw.write("hello");
+        }catch(IOException e){
+            System.out.println(e.getMessage());
             System.out.println("エラーが発生しました");
+        }finally{
+            fw.close();
         }
     }
 }
