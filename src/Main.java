c29e0fac4cfa1523a7997925467f549e6b56851a
Kota Takeda <xxk0ta0605@gmail.com>
2018-06-23 13:07:32 +0000

    p.597

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,14 @@
 public class Main {
     public static void main (String[] args) {
-        Person p = new Person();
-        p.setAge(-12);
+        String s = null;
+        try{
+            System.out.println(s.length());
+        }catch(NullPointerException e){
+            System.out.println("NullPointException 例外をcatchしました");
+            System.out.println("--スタックトレースはここから--");
+            e.printStackTrace();
+            System.out.println("--スタックトレースはここまで--");
+        }
+        
     }
 }
