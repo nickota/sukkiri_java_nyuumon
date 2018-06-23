f52cb9b0f4a459461e461dd4d602674a9ac5c038
Kota Takeda <xxk0ta0605@gmail.com>
2018-06-23 07:44:57 +0000

    p.544

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,5 @@
-import java.text.SimpleDateFormat;
-import java.util.Date;
-
 public class Main {
-    public static void main (String[] args) throws Exception {
-        
-        //本日の日時の表示
-        Date now = new Date();
-        SimpleDateFormat f = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
-        
-        String s = f.format(now);
-        System.out.println(s);
-        
-        //指定日時の文字列を解析しDate型として得る
-        Date d = f.parse("2011/09/22 01:23:45");
-    }
+    public void printAnything(Object o) {
+        //何型でも表示することができる
+        System.out.println(o);
 }
